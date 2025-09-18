beb1217a5758b3a06f831b144e949dec86f9f38a
quesitopre <citlallimartinez122@gmail.com>
2025-09-17 18:46:28 -0700

    made edits + \n

--- jeSuite.py
+++ jeSuite.py
@@ -1,2 +1,2 @@
-print("please add a new line of code and push & commit to repo for confirmation that it worked.")
-print("Date: 09/15")
+print("please add a new line of code by pushing the comitted changes to the jeSuite repo.")
+print("This will be confirmation that you have sucessfully cloned and can log the changes unto Github.")
