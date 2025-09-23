21c6d43f06fd1144ab25153e83751a9037f230c1
AngelaFernandez12 <angcrisfer@gmail.com>
2025-09-22 20:42:24 -0700

    first commit

--- jeSuite.py
+++ jeSuite.py
@@ -1,2 +1,3 @@
 print("please add a new line of code by pushing the comitted changes to the jeSuite repo.")
 print("This will be confirmation that you have sucessfully cloned and can log the changes unto Github.")
+print("hi")
